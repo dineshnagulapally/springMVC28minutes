153dd238d017ee3ba2ae196d7d2436e034862264
Dini <dineshnagulapally@gmail.com>
2022-07-09 09:37:55 -0500

    MVC

---
+++ src/main/java/com/example/springmvc20minutes/SpringMvc20minutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvc20minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc20minutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc20minutesApplication.class, args);
+    }
+
+}
